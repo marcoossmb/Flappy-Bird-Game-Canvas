809fea09a3a7906ffda7c8b83250bafd257e1d8a
marcoossmb_ <marcossblazquezz@gmail.com>
2024-01-04 12:07:40 +0100

    improved collision logic

--- js/script.js
+++ js/script.js
@@ -72,26 +72,29 @@ document.addEventListener("keyup", (event) => {
 
 const verificarColision = () => {
     for (let i = 0; i < obstaculosArray.length; i++) {
-        if (bird.x < obstaculosArray[i].x + obstaculosArray[i].ancho &&
-            bird.x + bird.ancho > obstaculosArray[i].x && 
-            ((bird.y < 0 + obstaculosArray[i].top && bird.y + bird.alto > 0) ||
-            (bird.y > canvasAlto - obstaculosArray[i].bottom &&
-            bird.y + bird.alto < canvasAlto))) 
-            {
-            clearInterval(juegoEnMarcha);
-            ctx.font= "25px Georgia";
-            ctx.fillStyle = "black";
-            ctx.fillText("Game Over, tu puntuación es "+score, 160, canvasAlto/2 -10)
-            
-            let btn_volver = document.createElement("button")
-            btn_volver.id = "btn_volver"
-            btn_volver.textContent = "Volver a Jugar"
-            btn_volver.classList.add("btn__volver")
-            document.body.appendChild(btn_volver)
+        const obstaculo = obstaculosArray[i];
+
+        // Verificar colisión en el eje X
+        if (bird.x < obstaculo.x + obstaculo.ancho &&
+            bird.x + bird.ancho > obstaculo.x) {
+            // Verificar colisión en el eje Y
+            if (bird.y < obstaculo.top ||
+                bird.y + bird.alto > canvasAlto - obstaculo.bottom) {
+                deadSound.play();
+                clearInterval(juegoEnMarcha);
+                ctx.font = "25px Georgia";
+                ctx.fillStyle = "black";
+                ctx.fillText("Game Over, tu puntuación es " + score, 160, canvasAlto / 2 - 10);
+
+                let btn_volver = document.createElement("button");
+                btn_volver.id = "btn_volver";
+                btn_volver.textContent = "Volver a Jugar";
+                btn_volver.classList.add("btn__volver");
+                document.body.appendChild(btn_volver);
+            }
         }
     }
 }
-
 
 document.addEventListener("click", (event) => {
     if (event.target.id === "btn_volver") {
